f8d0e548fba4e06b40f79e267ce143634a771299
xNihad <nxankishiyev@std.beu.edu.az>
2024-07-31 12:13:04 +0400

    Finished

--- public/web.js
+++ public/web.js
@@ -1,34 +1,60 @@
-let BaseUrl="http://localhost:7070/rooms/"
+let BaseUrl="http://localhost:7070/rooms"
 let cardsDiv = document.querySelector(".products-container")
-
+let addForm=document.querySelector(".add-form")
+let nameInp=document.querySelector(".nameInp")
+let descInp=document.querySelector(".descInp")
 
 async function GetDatas() {
     let datas = await axios.get(BaseUrl)
     try {
-        console.log(datas.data);
         CreateCards(datas.data)
     } catch (error) {
         console.log(error);
     }
 }
-GetDatas()
+
 
 function CreateCards(cards) {
+    cardsDiv.innerHTML=""
     cards.forEach(element => {
         cardsDiv.innerHTML+=
         `
-        <div class="card">
+        <div class="cardd">
           <div class="card-top">
              <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" class="image">
            </div>
-         <div class="card-bottom">
+           <div class="card-bottom">
             <h3>${element.name}</h3>
-            <span>${element.description}</span>
-          <button>View</button>
+            <div>${element.description}</div>
+          <button class="del-btn" btn-id=${element._id}>Delete</button>
       </div>
   </div>
-
-        `
+     `
+     let delBtns = document.querySelectorAll(".del-btn")
+     delBtns.forEach(btn => {
+        btn.addEventListener("click", async function(){
+            let id = btn.getAttribute("btn-id")
+           await axios.delete(`${BaseUrl}/${id}`)
+           GetDatas()
+        })
     });
-    
+
+    }); 
 }
+
+addForm.addEventListener("submit",async function(event){
+    event.preventDefault()
+    let newPro = {
+        name: nameInp.value,
+        description: descInp.value
+    } 
+    await axios.post(`${BaseUrl}`, newPro)
+     nameInp.value=""
+    descInp.value=""
+    GetDatas()
+})
+
+
+GetDatas()
+
+
